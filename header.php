36967055cdb616b4d3be95d69befcd175054304d
Vince Law <vince@employvince.com>
2013-07-03 15:00:53 +1000

    Version 1.1.2
    New
    
    - Fixed viewport meta tag in header.php by replacing ";" with ",".
    - Fixed jquery-fix-height.js by using window.load instead of document.ready.

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
 <title><?php the_title(); ?></title>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<meta name="viewport" content="width=device-width; initial-scale=1.0; minimum-scale=1.0; user-scalable=yes;" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, user-scalable=yes" />
 <meta property="og:site_name" content="<?php bloginfo('name'); ?>" />
 <?php
 #FACEBOOK SETTINGS FOR BLOG POSTS
--- js/jquery-fix-height.js
+++ js/jquery-fix-height.js
@@ -1,12 +1,18 @@
 //WAIT FOR PAGE TO LOAD
-$(document).ready(function()
-{
+$(window).load(function()
+{	
 	//INITIALISE THE CONTENT & SIDEBAR HEIGHTS
-	content_height = $('#content').height();
+	content_height = $('#content').outerHeight(true);
 	sidebar_height = $('#sidebar').height();
 	
 	//INITIALISE THE MAXIMUM HEIGHT
 	max_height = Math.max(content_height, sidebar_height);
+	
+	/*
+	console.log('Content: ' + content_height);
+	console.log('Sidebar: ' + sidebar_height);
+	console.log('Max: ' + max_height);
+	*/
 	
 	//CONTENT HEIGHT IS SHORTER THAN SIDEBAR HEIGHT
 	if(content_height < sidebar_height)
